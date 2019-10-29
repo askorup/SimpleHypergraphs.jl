d7f5459bfa87d0b1fbb9132c74f7462d2ff34b30
Carmine Spagnuolo <spagnuolocarmine@gmail.com>
2019-10-29 11:11:54 +0100

    Update widgets.jl

--- src/visualization/widgets.jl
+++ src/visualization/widgets.jl
@@ -3,29 +3,41 @@ struct Widget
     body
 end
 
-function widgetColorEdge(data,Preferences,idColorEdge)
+function widgetColorEdge(data,preferences,idColorEdge)
     w= Widget("colorEdge", """
     <head>
     <link rel="stylesheet" type="text/css" href="https://isislab-unisa.github.io/hypergraphs-plot/css/color-edge-style.css">
     </head>
     <div class="hg-plot"""*string(idColorEdge)*""""></div>
-    <script src="https://isislab-unisa.github.io/hypergraphs-plot/bundle.v1.0.js"></script>
+    <script src="https://isislab-unisa.github.io/hypergraphs-plot//bundle.v1.0.js"></script>
     <script>
-    hgplot.drawing.hgColorEdgePlot({graph:"""*data*"""},{Preferences:"""*JSON.json(Preferences)*"""},{idColorEdge:"""*"\""*string(idColorEdge)*"\""*"""});
+    hgplot.drawing.hgColorEdgePlot({graph:"""*data*"""},{preferences:"""*JSON.json(preferences)*"""},{idColorEdge:"""*"\""*string(idColorEdge)*"\""*"""});
     </script>
     """)
     return w
 end
 
+function widgetRadal(data,preferences,idRadal)
+    w= Widget("radal", """
+    <head>
+    <link rel="stylesheet" type="text/css" href="https://isislab-unisa.github.io/hypergraphs-plot/css/radal-style.css">
+    </head>
+    <div class="radarChart"""*string(idRadal)*""""></div>
+    <script src="https://isislab-unisa.github.io/hypergraphs-plot//bundle.v1.0.js"></script>
+    <script>
+    hgplot.drawing.hgRadalPlot({graph:"""*data*"""},{preferences:"""*JSON.json(preferences)*"""},{idRadal:"""*"\""*string(idRadal)*"\""*"""});;
+    </script>
+    """)
+    return w
+end
 
-#<script src="./bundle.v1.0.js"></script>
 function widgetVenn(data)
     w= Widget("venn", """
     <head>
     <link rel="stylesheet" type="text/css" href="https://isislab-unisa.github.io/hypergraphs-plot/css/venn-style.css">
     </head>
     <div id="venn"></div>
-    <script src="https://isislab-unisa.github.io/hypergraphs-plot/bundle.v1.0.js"></script>
+    <script src="https://isislab-unisa.github.io/hypergraphs-plot//bundle.v1.0.js"></script>
     <script>
     hgplot.drawing.hgVennNodesPlot({graph:"""*data*"""});
     </script>
@@ -34,20 +46,7 @@ function widgetVenn(data)
     return w
 end
 
-function widgetRadal(data)
-    w= Widget("radal", """
-    <head>
-    <link rel="stylesheet" type="text/css" href="https://isislab-unisa.github.io/hypergraphs-plot/css/radal-style.css">
-    </head>
-    <div class="radarChart"></div>
-    <script src="https://isislab-unisa.github.io/hypergraphs-plot/bundle.v1.0.js"></script>
-    <script>
-    hgplot.drawing.hgRadalPlot({graph:"""*data*"""})
-    </script>
-    """)
 
-    return w
-end
 
 function display(w::Widget)
     HTML(w.body)
