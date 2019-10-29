35ef0e017f772f93ac7c9a99d4f5fd8dda4bb816
Carmine Spagnuolo <spagnuolocarmine@gmail.com>
2019-10-29 11:05:20 +0100

    Update plot.jl

--- src/visualization/plot.jl
+++ src/visualization/plot.jl
@@ -1,25 +1,31 @@
+using PyCall
 #=
 plot hypergraphs with different visualizations
+
 layout: color-edge, venn, radal
+
 params: h: Hyperhgraph
         type: color-edge(default), venn, radal
+
 example: plot(h, type="color-edge")
          plot("data.json", type="venn")
 =#
-idColorEdge=0
 
-function plot(JSONString::String;type="color-edge",colorNodes="#D3D3D3",colorEdges="default",sizeNodes=10)
-    Preferences=Dict()
-    Preferences["colorNodes"]=colorNodes;
-    Preferences["colorEdges"]=colorEdges;
-    Preferences["sizeNodes"]=sizeNodes;
+idColorEdge=0
+idRadal=0
+
+function plot(JSONString::String;type="color-edge",colorNodes="default",colorEdges="default",sizeNodes="default")
+    preferences=Dict()
+    preferences["colorNodes"]=colorNodes;
+    preferences["colorEdges"]=colorEdges;
+    preferences["sizeNodes"]=sizeNodes;
 
     data = conversionJSON(JSONString)
 
-    plotting(data,Preferences,type)
+    plotting(data,preferences,type)
 end
 
-function plot(h::Hypergraph;type="color-edge",colorNodes="#D3D3D3",colorEdges="default",sizeNodes=10)
+function plot(h::Hypergraph;type="color-edge",colorNodes="default",colorEdges="default",sizeNodes="default")
     preferences=Dict()
     preferences["colorNodes"]=colorNodes;
     preferences["colorEdges"]=colorEdges;
@@ -34,13 +40,14 @@ function plot(h::Hypergraph;type="color-edge",colorNodes="#D3D3D3",colorEdges="d
 
 end
 
-function plotting(data,Preferences,type)
+function plotting(data,preferences,type)
     if type=="venn"
         plotVenn(data)
     elseif type=="color-edge"
         global idColorEdge=idColorEdge+1
-        plotColorEdge(data,Preferences,idColorEdge)
+        plotColorEdge(data,preferences,idColorEdge)
     elseif type=="radal"
-        plotRadal(data)
+        global idRadal=idRadal+1
+        plotRadal(data,preferences,idRadal)
     end
 end
